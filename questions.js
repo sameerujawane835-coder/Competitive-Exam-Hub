8f4669eeeb753fb3ac7a5e3de371b58126cb3480
sameerujawane835-coder <sameerujawane835@gmail.com>
2026-07-13 16:15:55 +0530

    Add questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: "Maharashtra ki Rajdhani kya hai?",
+    options: ["Pune", "Mumbai", "Nagpur", "Nashik"],
+    answer: 1
+  },
+  {
+    question: "Bharat ka Rashtriya Pashu kaun sa hai?",
+    options: ["Sher", "Bagh", "Hathi", "Mor"],
+    answer: 1
+  },
+  {
+    question: "15 × 12 = ?",
+    options: ["160", "170", "180", "190"],
+    answer: 2
+  },
+  {
+    question: "Bharat ka Samvidhan kab lagu hua?",
+    options: [
+      "26 January 1950",
+      "15 August 1947",
+      "26 November 1949",
+      "2 October 1950"
+    ],
+    answer: 0
+  },
+  {
+    question: "Maharashtra me kitne jile hain?",
+    options: ["34", "35", "36", "37"],
+    answer: 2
+  }
+];
