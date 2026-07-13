92d41eba84256957d2f919321b0c5f2f0af7589f
sameerujawane835-coder <sameerujawane835@gmail.com>
2026-07-13 18:10:06 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -28,5 +28,30 @@ const questions = [
     question: "Maharashtra me kitne jile hain?",
     options: ["34", "35", "36", "37"],
     answer: 2
+  },
+  {
+    question: "Maharashtra ka sabse bada shahar kaun sa hai?",
+    options: ["Nagpur", "Pune", "Mumbai", "Nashik"],
+    answer: 2
+  },
+  {
+    question: "Bharat ke pehle Rashtrapati kaun the?",
+    options: ["Dr. Rajendra Prasad", "Nehru", "Gandhi", "Ambedkar"],
+    answer: 0
+  },
+  {
+    question: "Police Bharti me physical test ka ek important part kya hai?",
+    options: ["Running", "Swimming", "Cycling", "Driving"],
+    answer: 0
+  },
+  {
+    question: "Bharat ka Rashtriya Phool kaun sa hai?",
+    options: ["Gulab", "Kamal", "Surajmukhi", "Genda"],
+    answer: 1
+  },
+  {
+    question: "Maharashtra ki official language kya hai?",
+    options: ["Hindi", "English", "Marathi", "Gujarati"],
+    answer: 2
   }
 ];
